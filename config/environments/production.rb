809e8c3b34ccf9f1847dc4ea2a35ae8fb3ecdecd
Denis280303 <kozlovskyidenus@gmail.com>
2023-03-30 20:44:22 +0300

    Redis changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,13 +61,21 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "expenses_tracker_production"
-
-  config.action_mailer.perform_caching = false
+  #
+  config.action_mailer.default_url_options = {host: "expenses-own-tracker.herokuapp.com", protocol: "https"}
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'email-smtp.eu-north-1.amazonaws.com',
+    port:                 587,
+    user_name:            'AKIA2PDTBO6Z3DN67EOH',
+    password:             'BC4ar2GghzbhnrvG6/KQToedw/mRsdnShL3FoPVM1y7x',
+    authentication:       :plain,
+    enable_starttls_auto: true }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.raise_delivery_errors = true
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
